fa4acdffe7baeec48fad9d9269c737894f96cd5c
thoufiq85 <84376022+thoufiq85@users.noreply.github.com>
2023-04-23 13:25:01 +0530

    Delete Solidity2_2.sol

--- Solidity2_2.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract MyContract {
-    string public name="Tofiq";
-  function getName() public view returns (string memory) {
-    return name;
-  }
-}
